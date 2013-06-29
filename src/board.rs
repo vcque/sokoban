df154e20ad20021d4b96ffc86db00de495ed3bb7
vocquet <vincent.ocquet@gmail.com>
2013-06-29 19:42:36 +0200

    implementing flow mask algorithm + test

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,6 @@
 use std::int;
 use std::vec;
+use std::str::push_str;
 
 /// The interface a sokoban game implements.
 trait Game {
@@ -62,6 +63,7 @@ impl Tile {
 /*
 The mask data structure.
 */
+#[deriving(Eq)]
 struct Mask { data: ~[~[bool]] }
 
 impl Mask {
@@ -114,6 +116,20 @@ impl Mask {
     
 }
 
+impl ToStr for Mask {
+    fn to_str(&self) -> ~str {
+        let mut result = ~"";
+        for self.data.each |col| {
+            for col.each |b| {
+                if *b {push_str(&mut result, "1");}
+                else {push_str(&mut result, "0");}
+            }
+            push_str(&mut result, "\n");
+        }
+    result
+    }
+}
+
 struct Board { data: ~[~[Tile]] }
 
 pub struct Position {
@@ -122,10 +138,41 @@ pub struct Position {
 }
 
 impl Position {
-    priv fn flow_mask(&self, mask: &mut Mask, (i,j): (int, int)) {
-        
+    priv fn flow_mask(&self, mask: &mut Mask, (i,j): (uint, uint)) {
+        // x axis.
+        let xl = self.get_tile(i-1, j);
+        let xr = self.get_tile(i+1, j);
+        let xlm = mask.data[i-1][j];
+        let xrm = mask.data[i+1][j];
+        if ((xl.is_passable() || xlm) && (xr.is_passable() || xrm)) {
+            if (!xlm) {
+                mask.data[i-1][j] = true;
+                self.flow_mask(mask, (i-1, j));
+            }
+            if (!xrm) {
+                mask.data[i+1][j] = true;
+                self.flow_mask(mask, (i+1, j));
+            }
+        }
+
+        // y axis.        
+        let yl = self.get_tile(i, j-1);
+        let yr = self.get_tile(i, j+1);
+        let ylm = mask.data[i][j-1];
+        let yrm = mask.data[i][j+1];
+        if ((yl.is_passable() || ylm) && (yr.is_passable() || yrm)) {
+            if (!ylm) {
+                mask.data[i][j-1] = true;
+                self.flow_mask(mask, (i, j-1));
+            }
+            if (!yrm) {
+                mask.data[i][j+1] = true;
+                self.flow_mask(mask, (i, j+1));
+            }
+        }
     }
 }
+
 impl Game for Position {
 
     fn from_win(&self) -> uint {
@@ -142,7 +189,12 @@ impl Game for Position {
     }
     
     fn get_tile(&self, x: uint, y: uint) -> Tile {
-        self.board.data[x][y]
+        let (n,m) = self.size();
+        if ( x < 0  || y < 0 || x >= n || y >= m ) {
+            Wall
+        } else {
+            self.board.data[x][y]
+        }
     }
     
     fn get_box_number(&self) -> uint {
@@ -163,7 +215,11 @@ impl Game for Position {
     fn get_box_mask(&self, box: uint) -> Option<~Mask> {
         let mut mask = Mask::new(self.board.data, |til| { *til == Box(box) });
         if (mask.occurences() == 0) { return None; }
-        None
+        else {
+            let pos = self.get_box_position(box);
+            self.flow_mask(mask, pos.get());
+            Some(mask)
+        }
     }
     
     
@@ -182,5 +238,34 @@ fn test_tile() {
     assert!(Box(1) != Box(2));
     assert!(Box(1) != Wall);
     assert!(Box(0).is_box());
+}
+
+#[test]
+fn test_flow_mask() {
+    let mut tiles = vec::with_capacity(10);
+    for int::range(0,10) |x| {
+        let col = vec::from_fn(10, |i| {
+            match (x, i) {
+            (5,5) => {Box(0)},
+            (0,_) => {Wall},
+            (9,_) => {Wall},
+            (_,0) => {Wall},
+            (_,9) => {Wall},
+            // (1,3) => {Floor},
+            // (_,3) => {Wall},
+            (_,_) => {Floor}
+            }
+        });
+        tiles.push(col);
+    }
+
+    let b = Board{data: tiles};
+    let pos = Position{board: b, player: (0,0)};
+    let flowMask = pos.get_box_mask(0);
+    let mask = Mask::new(pos.board.data, |til| {
+        *til != Wall
+    });
+    
+    assert!(flowMask.get() == mask);
 }
 
